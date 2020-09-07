76a4aeb7f6056484027df1f2e3a8fe0c0e918cc9
mosesbenjamin <mavewreek@gmail.com>
2020-09-07 08:29:12 +0100

    variable declaration, types and intro

---
+++ main.ts
@@ -0,0 +1,40 @@
+export {}
+let message = "Welcome back";
+console.log(message)
+
+let x=10;
+const y=30;
+
+let sum;
+const title = "Mavewrick";
+
+let isBeginner: boolean = true;
+let total:number = 0;
+let name: string = "Moses";
+
+let sentence: string = `My name is ${name}
+I am a beginner in typescript`;
+
+console.log(sentence);
+
+let n: null = null;
+let u:undefined = undefined;
+
+let isNew: boolean = null;
+let myName:string = undefined;
+
+
+let list1: number[] = [1,2,3];
+let list2: Array<number> = [1,2,3];
+
+
+// Tuples: Fixed 
+let person1: [string, number] = ["Chris", 22];
+
+// Enum
+
+enum Color {Red=5, Green, Blue};
+
+let c:Color = Color.Green;
+console.log(c)
+
