fe3ef9a382f52abebb364ce4db6f6c214173fa08
mosesbenjamin <mavewreek@gmail.com>
2020-09-21 15:29:22 +0100

    Class/Stateful component syntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import Message from './components/Message';
 
-function App() {
+class App extends Component<any>{
+  componentWillMount(){
+    console.log('Almost there');
+  }
 
+  componentDidMount(){
+    console.log('Finally...hello!');
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Message message='This is a simple message'/>
-      </header>
-    </div>
-  );
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Message message='This is a simple message'/>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
