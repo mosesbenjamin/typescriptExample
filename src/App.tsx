f5a28e42c8ef0e6fc4d79af0097decee4f072714
mosesbenjamin <mavewreek@gmail.com>
2020-09-21 15:01:53 +0100

    Basic types

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // let firstValue: string = 'Many';
+  // let firstValue: number = 23;
+  // let firstValue: boolean = true;
+  // let firstValue: number[] = [2, 3, 4];
+  // let firstValue: Array<string> = ['2', '3', '4'];
+
+  // // tuple
+  // let aTuple: [string, number] =  ['Many', 5];
+
+  // //  enum
+  // enum Codes {first, second};
+
+  // any
+  let firstName: any = 4;
+
+  // //  void
+  // const warning = (): void=>{
+  //   console.log('Warning')
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          The value {firstName} is of { typeof firstName} type!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
